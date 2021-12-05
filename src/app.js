1de2d0064087fd3cfa3e14a61e084e8c247308ea
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-12-05 00:21:02 -0500

    refactoring app.js

--- src/app.js
+++ src/app.js
@@ -6,17 +6,10 @@ const userRoutes = require('./resources/route/userRouter.js')
 const boardRouter = require('./resources/route/boardRouter.js')
 const taskRouter = require('./resources/route/taskRouter.js')
 
-
-userRoutes.forEach((route) => {
+const routes = [userRoutes, boardRouter, taskRouter]
+routes.forEach(r => r.forEach((route) => {
   app.route(route)
-})
+}))
 
-boardRouter.forEach((route) => {
-  app.route(route)
-})
-
-taskRouter.forEach((route) => {
-  app.route(route)
-})
 
 module.exports = app;
