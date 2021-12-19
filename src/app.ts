502fd024833bfac4466b6fd9aacc9cf665494da1
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-12-18 22:43:17 -0500

    add TSDoc to user.service.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ const app = require('fastify')({
   logger: true
 });
 
-const userRouter = require('./resources/route/userRouter.ts')
-const boardRouter = require('./resources/route/boardRouter.ts')
-const taskRouter = require('./resources/route/taskRouter.ts')
+const userRouter = require('./resources/route/userRouter')
+const boardRouter = require('./resources/route/boardRouter')
+const taskRouter = require('./resources/route/taskRouter')
 
 const routes = [userRouter, boardRouter, taskRouter]
+
+
 routes.forEach(r => r.forEach((route: object) => {
   app.route(route)
 }))
--- src/resources/service/user.service.ts
+++ src/resources/service/user.service.ts
@@ -1,16 +1,49 @@
 const usersRepo = require('../repository/user.repository.ts');
 
-
+/**
+ * Returns users list
+ * @returns Users list
+ */
 const getAllUsersService = () => usersRepo.getAll();
+
+/**
+ * Get a user by id
+ * @param userId - user's id
+ * @returns get a user by id
+ */
 
 const getOneUserService = (userId: string) => usersRepo.getOne(userId);
 
+/**
+ * Add a new user
+ * @param user - object with user parameters
+ * @returns New user object with ID
+ */
 const addUserService = (user: object) => usersRepo.addUserRepo(user);
+
+/**
+ * Update a user by id
+ * @param userId - user's id
+ * @param body - object with a new user parameters
+ * @returns Updated user object with
+ */
 
 const updateUserService = (userId: string, body: object) => usersRepo.updateUserRepo(userId, body);
 
+/**
+ * Delete a user by id
+ * @param userId - user's id
+ * @returns Delete a user by id
+ */
 
 const deleteUserService = (userId: string) => usersRepo.deleteUserRepo(userId);
+
+
+/**
+ * Return user without password
+ * @param user - user's object
+ * @returns User without password
+ */
 
 function returnUserWithoutPass(user: { id: string; name: string; login: string; }) {
     const { id, name, login } = user;
