eca4b6eae8e1c50758252f69ae3a7aa080ab386f
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-12-25 17:43:04 -0500

    add log files

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 const app = require('fastify')({
-  logger: true
+  logger: [{
+    level: 'error',
+    file: './logs/error.log'
+  }, {
+    level: 'info',
+    file: './logs/info.log'
+  }],
+
+
 });
 
 const userRouter = require('./resources/route/userRouter')
@@ -8,9 +16,12 @@ const taskRouter = require('./resources/route/taskRouter')
 
 const routes = [userRouter, boardRouter, taskRouter]
 
+app.log.error('Example error log');
 
 routes.forEach(r => r.forEach((route: object) => {
   app.route(route)
 }))
+
+
 
 module.exports = app;
