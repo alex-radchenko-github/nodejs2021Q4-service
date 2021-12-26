6db92e566ef1d12b2b894597b4a491bfb72e8663
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-12-25 19:48:58 -0500

    add uncaughtException ans unhandledRejection

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
+// trace, debug, info, warn, error, fatal
+
 const app = require('fastify')({
-  logger: [{
+  logger: {
     level: 'error',
     file: './logs/error.log'
-  }, {
-    level: 'info',
-    file: './logs/info.log'
-  }],
-
+  }
 
 });
+
 
 const userRouter = require('./resources/route/userRouter')
 const boardRouter = require('./resources/route/boardRouter')
@@ -16,12 +15,50 @@ const taskRouter = require('./resources/route/taskRouter')
 
 const routes = [userRouter, boardRouter, taskRouter]
 
-app.log.error('Example error log');
+app.addHook('preHandler', (req: { body: object; log: { info: (arg0: { body: object; }, arg1: string) => void; }; }, reply: object, done: () => void) => {
+  if (req.body) {
+    req.log.info({ body: req.body }, 'parsed body')
+  }
+  done()
+})
+
+// trace, debug, info, warn, error, fatal
+
+// // Example for trace log
+// app.log.trace('Example for trace log');
+//
+// // Example for debug log
+// app.log.debug('Example for debug log');
+//
+// // Example for info log
+// app.log.info('Example for info log');
+//
+// // Example for warn log
+// app.log.warn('Example for warn log');
+//
+// // Example for error log
+// app.log.error('Example for error log');
+//
+// // Example for fatal log
+// app.log.fatal('Example for fatal log');
+
+process.on('uncaughtException', (e) => {
+    app.log.fatal(`${e.message}`)
+});
+
+// throw Error('Oops!');
+
+process.on('unhandledRejection', (e) => {
+    app.log.fatal(`${e}`)
+});
+
+// Promise.reject(Error('Oops!'));
+
 
 routes.forEach(r => r.forEach((route: object) => {
   app.route(route)
 }))
 
 
-
 module.exports = app;
+
