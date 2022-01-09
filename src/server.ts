310c2b33ae61a1cf3831d6301fed0c91e0ccb81f
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2022-01-08 21:12:08 -0500

    add docker-compose

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ const { PORT } = require('./common/config.ts');
 const application = require('./app.ts');
 
 
-
-application.listen(PORT, () =>
+application.listen(PORT, '0.0.0.0',() =>
     application.log.info(`App is running on http://localhost:${PORT}`)
 );
