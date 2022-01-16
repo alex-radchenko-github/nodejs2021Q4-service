36d784a5c8b861f00b9de7d47209f74f1f942960
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2022-01-16 12:19:20 -0500

    add conection var to .env

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,12 +2,19 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({
-  path: path.join(__dirname, '../../.env')
+    path: path.join(__dirname, '../../.env')
 });
 
 
 const PORT = Number(process.env.PORT)
-const { NODE_ENV } = process.env;
+const {NODE_ENV} = process.env;
+const PGHOST = process.env.POSTGRES_HOST
+const PGPORT = Number(process.env.POSTGRES_PORT)
+const PGUSER = process.env.POSTGRES_USER
+const PGPASSWORD = process.env.POSTGRES_PASSWORD
+const PGDNNAME = process.env.POSTGRES_DB
+const SYNCHRONIZEMODE = Boolean(process.env.SYNCHRONIZE_MODE)
+const LOGGINGMODE = Boolean(process.env.LOGGING_MODE)
 
 
 // module.exports = {
@@ -19,4 +26,4 @@ const { NODE_ENV } = process.env;
 // };
 
 
-export { PORT, NODE_ENV };
+export {PORT, NODE_ENV, PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDNNAME, SYNCHRONIZEMODE, LOGGINGMODE};
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,25 @@
 import {createConnection} from "typeorm";
 
+import "reflect-metadata";
 
-
-// import "reflect-metadata";
-
-import {PORT} from './common/config';
+import {PORT, PGHOST, PGPORT, PGUSER, PGPASSWORD, PGDNNAME, SYNCHRONIZEMODE, LOGGINGMODE} from './common/config';
 
 require('dotenv').config()
 
 const application = require('./app.ts');
 
 const start = async () => {
-    // const connection
     await createConnection({
         type: "postgres",
-        host: "postgres",
-        port: 5432,
-        username: "postgres",
-        password: "postgres",
-        database: "nodejs2021Q4Service",
+        host: PGHOST,
+        port: PGPORT,
+        username: PGUSER,
+        password: PGPASSWORD,
+        database: PGDNNAME,
         entities: ["src/entity/*.ts"],
         migrations: ["src/migration/**/*.ts"],
-        synchronize: true,
-        logging: false
+        synchronize: SYNCHRONIZEMODE,
+        logging: LOGGINGMODE
     });
 
     application.listen(PORT, '0.0.0.0', () =>
